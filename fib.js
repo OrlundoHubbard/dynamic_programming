eb568e3ca2f6b5d653e793e3755f59a3d5635044
Orlundo Hubbard <orlundo@wursta.com>
2022-11-21 17:47:56 -0600

    more dynamic programming

--- fib.js
+++ fib.js
@@ -1,26 +1,32 @@
-const fib = (n) => {
+// memoization
+// js object, keys will be arg to fn, value will be the return value
+
+const fib = (n, memo = {}) => {
+    if (n in memo) return memo[n];
     if (n <= 2) return 1;
-    return fib(n - 1) + fib(n -2);
+    memo[n] = fib(n - 1, memo) + fib(n -2);
+    return memo[n];
 };
 
-const bar = (n) => {
-    if (n <= 1) return;
-    bar(n -2); // O(n) time, O(n) space
-}
+// const bar = (n) => {
+//     if (n <= 1) return;
+//     bar(n -2); // O(n) time, O(n) space
+// }
 
-const dib = (n) => {
-    if (n <= 1) return;
-    dib(n - 1);
-    dib(n -1);
-}
+// const dib = (n) => {
+//     if (n <= 1) return;
+//     dib(n - 1);
+//     dib(n -1);
+// }
 
-const lib = (n) => {
-    if (n <= 1) return;
-    lib(n - 2);
-    lib(n - 2);
-}
+// const lib = (n) => {
+//     if (n <= 1) return;
+//     lib(n - 2);
+//     lib(n - 2);
+// }
 
 
-console.log(fib(2));
-console.log(fib(3));
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
 console.log(fib(50));
