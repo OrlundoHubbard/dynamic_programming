08dfb398330f870842a81c9415b6e95b896de342
Orlundo Hubbard <orlundo@wursta.com>
2022-11-21 03:40:16 -0600

    more practice

--- fib.js
+++ fib.js
@@ -3,6 +3,24 @@ const fib = (n) => {
     return fib(n - 1) + fib(n -2);
 };
 
+const bar = (n) => {
+    if (n <= 1) return;
+    bar(n -2); // O(n) time, O(n) space
+}
+
+const dib = (n) => {
+    if (n <= 1) return;
+    dib(n - 1);
+    dib(n -1);
+}
+
+const lib = (n) => {
+    if (n <= 1) return;
+    lib(n - 2);
+    lib(n - 2);
+}
+
+
 console.log(fib(2));
 console.log(fib(3));
 console.log(fib(50));
