4a0ae44d0be5f4f3d8d6592da1f3ac6d3d1b8c18
Orlundo Hubbard <orlundo@wursta.com>
2022-11-26 13:12:22 -0600

    more js

--- gridTraveler.js
+++ gridTraveler.js
@@ -1,8 +1,16 @@
-const gridTraveler = (m, n) => {
+const gridTraveler = (m, n, memo={}) => {
+    const key = m + ',' + n;
+if (key in memo) return memo[key];
  if (m === 1 && n === 1) return 1;
  if (m === 0 || n === 0) return 0;
- return gridTraveler(m-1, n) + gridTraveler(m, n -1);
+ memo[key] =  gridTraveler(m-1, n) + gridTraveler(m, n -1);
+ return memo[key];
 };
+
+// m = 42
+// n = 3
+
+// key = 
 
 console.log(gridTraveler(1, 1)); // 1
 console.log(gridTraveler(2, 3)); // 3
