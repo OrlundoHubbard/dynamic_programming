b3684571fdf04d535ff2927ce31335b8f8aec0b8
Orlundo Hubbard <orlundokhubbard@gmail.com>
2022-12-04 19:52:45 -0600

    canSum

---
+++ can-sum.js
@@ -0,0 +1,9 @@
+const canSum = (targetSum, numbers) => {
+  if (targetSum === 0) return true;
+};
+
+console.log((7, [2, 3]));
+console.log((7, [5, 3, 4, 7]));
+console.log((7, [2, 4]));
+console.log((8, [2, 3, 5]));
+console.log((300, [7, 14]));
